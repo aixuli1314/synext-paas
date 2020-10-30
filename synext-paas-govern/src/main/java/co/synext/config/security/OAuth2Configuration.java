c882f6143e9943640552e828400f1f7965a7fb9b
aixuli1314 <108145462@qq.com>
2020-10-30 14:48:18 +0800

    @Lazy

--- synext-paas-govern/src/main/java/co/synext/config/security/OAuth2Configuration.java
+++ synext-paas-govern/src/main/java/co/synext/config/security/OAuth2Configuration.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -66,6 +67,7 @@ public class OAuth2Configuration {
     public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
         @Autowired
+        @Lazy
         private AuthenticationManager authenticationManager;
 
         @Autowired
